d6eb9f7267a9cfb53465a06619c34a8c342c15de
danilovalerio <danilo_vs@hotmail.com>
2018-11-05 18:50:12 -0200

    Função lambda como parâmetro da lista.sort

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,11 +15,8 @@ public class Programa {
 		stock.add(new Produto("Notebook", 1200.0));
 		stock.add(new Produto("Tablet", 400.0));
 		
-		//Implementação via Lambda, torna menos verbosa (arrow function do JS)
-		//o compilador faz a inferencia de Produto para p1 e p2, assim dispensa a necessidade de colocar 'Produto p1'
-		Comparator<Produto> comp = (p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase()); //comparator numa única linha
-				
-		stock.sort(comp);
+		//Parâmetro passamos uma função Lâmbda, isto é, uma função anônima
+		stock.sort((p1, p2) -> p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase()));
 		
 		for(Produto p : stock) {
 			System.out.println(p);
