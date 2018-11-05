61b1204215a91d58d353eb35bcf0c0d41628b682
danilovalerio <danilo_vs@hotmail.com>
2018-11-05 18:42:23 -0200

    Terceia forma de implementar um Comparator com expressão Lambda

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -15,17 +15,11 @@ public class Programa {
 		stock.add(new Produto("Notebook", 1200.0));
 		stock.add(new Produto("Tablet", 400.0));
 		
-		Comparator<Produto> comp = new Comparator<Produto>() {
-			//Implementando comparator através de uma classe anônima 
-			
-			@Override
-			public int compare(Produto p1, Produto p2) {
-				return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
-			}
+		//Implementação via Lambda, torna menos verbosa (lembra o arrow function do JS)
+		Comparator<Produto> comp = (p1, p2) -> {
+			return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
 		};
 			
-		//Segunda forma de criar um comparator, assim
-		//se torna desnecessária a classse 'MyComparator'
 		stock.sort(comp);
 		
 		for(Produto p : stock) {
