57a4fea74315a394e5728faaa43c1e0c6d3cdc2d
danilovalerio <danilo_vs@hotmail.com>
2018-11-05 17:52:17 -0200

    Segunda forma de implementar um Comparator, sem a necessidade de se criar uma classe

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,6 +1,7 @@
 package aplicacao;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import entities.Produto;
@@ -14,7 +15,18 @@ public class Programa {
 		stock.add(new Produto("Notebook", 1200.0));
 		stock.add(new Produto("Tablet", 400.0));
 		
-		stock.sort(new MyComparator());
+		Comparator<Produto> comp = new Comparator<Produto>() {
+			//Implementando comparator através de uma classe anônima 
+			
+			@Override
+			public int compare(Produto p1, Produto p2) {
+				return p1.getNome().toUpperCase().compareTo(p2.getNome().toUpperCase());
+			}
+		};
+			
+		//Segunda forma de criar um comparator, assim
+		//se torna desnecessária a classse 'MyComparator'
+		stock.sort(comp);
 		
 		for(Produto p : stock) {
 			System.out.println(p);
